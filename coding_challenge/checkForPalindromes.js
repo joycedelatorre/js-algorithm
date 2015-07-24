35ee7790559c945e6389e35128ebaf358de74d84
joycedelatorre <joycedelatorre87@gmail.com>
2015-07-23 22:30:01 -0700

    add the third solution palindrome neither odd nor even

--- coding_challenge/checkForPalindromes.js
+++ coding_challenge/checkForPalindromes.js
@@ -64,7 +64,64 @@ function palindrome(str){
 }
 palindrome("A man, a plan, a canal. Panama");
 palindrome("race car");
-//palindrome("never odd or even");
 ----------------------------------
+// third solution
+function palindrome(str){
+  var myReg = /\w/g;
+  var word_array = str.match(myReg).join("").toLowerCase().split("");
+  var even_or_odd = Math.floor(word_array.length%2);
+  if (even_or_odd!==0){
+    //console.log(word_array);
+    middle = Math.floor(word_array.length/2);
+    word_holder1 =[];
+    for(var i = 0; i<=middle; i++){
+      word_holder1.push (word_array[i]);
+    }
+
+    word_holder1= word_holder1.join("");
+    //console.log(word_holder1);
+
+    word_holder2 =[];
+    for (var j = word_array.length-1; j>=middle; j--){
+      word_holder2.push(word_array[j]);
+    }
+    //console.log(word_holder2);
+    word_holder2= word_holder2.join("");
+
+    if (word_holder1 === word_holder2){
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+
+    middle = Math.floor((word_array.length/2)-1);
+    even_word_holder1 =[];
+    for (var i = 0; i<= middle; i++){
+      even_word_holder1.push(word_array[i]);
+    }
+    even_word_holder1= even_word_holder1.join("");
+
+    even_word_holder2 =[];
+    for (var j = str.length; j>=middle+1; j--){
+      even_word_holder2.push(word_array[j]);
+    }
+    even_word_holder2 = even_word_holder2.join("");
+    if (even_word_holder1 === even_word_holder2){
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+}
+palindrome("racecar");
+palindrome("eye");
+palindrome("kayak");
+palindrome("dad");
+palindrome("A man, a plan, a canal. Panama");
+palindrome("race car");
+palindrome("never odd or even");
 // there is a better solution for palindrome using regex only.
 
+//palindrome("never odd or even");
